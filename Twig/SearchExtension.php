00baa82a885b02390d2e0a99807059a5cb3e556b
Evheniy Bystrov <evheniy.bystrov@just-eat.com>
2015-04-03 17:31:04 +0300

    searcher

--- Twig/SearchExtension.php
+++ Twig/SearchExtension.php
@@ -27,6 +27,16 @@ class SearchExtension extends \Twig_Extension
     /**
      * @return array
      */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('url', array($this, 'filter')),
+        );
+    }
+
+    /**
+     * @return array
+     */
     public function getGlobals()
     {
         return array(
@@ -42,5 +52,40 @@ class SearchExtension extends \Twig_Extension
     public function getName()
     {
         return 'search';
+    }
+
+    /**
+     * @param array $url
+     * @param string $type
+     * @param string $key
+     * @param string $value
+     * @return array
+     */
+    public function filter(array $url, $type, $key, $value)
+    {
+        if (!in_array($type, array('add', 'remove'))) {
+            return $url;
+        }
+        if ($type == 'add') {
+            $url[$key][] = $value;
+        }
+
+        if ($type == 'remove') {
+            unset($url[$key][array_search($value, $url[$key])]);
+        }
+
+        $url[$key] = array_values($url[$key]);
+
+        $searcher = $this->container->get('search');
+
+        $url = $searcher->hierarchyLogic(
+            array(
+                'cities'    => $url['cities'],
+                'towns'     => $url['towns'],
+                'districts' => $url['districts']
+            )
+        );
+
+        return $url;
     }
 }
