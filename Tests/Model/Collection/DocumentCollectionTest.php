aede1d7265c6f31d584a044ad9daa07fc0866af4
Evheniy Bystrov <evheniy.bystrov@gmail.com>
2015-09-19 11:19:02 +0300

    test

---
+++ Tests/Model/Collection/DocumentCollectionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Evheniy\SearchBundle\Tests\Model\Collection;
+
+/**
+ * Class DocumentCollectionTest
+ *
+ * @package Evheniy\SearchBundle\Tests\Model\Collection
+ */
+class DocumentCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testConstruct()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     *
+     */
+    public function testGetEntities()
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ Tests/Model/Collection/FilterCollectionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Evheniy\SearchBundle\Tests\Model\Collection;
+
+/**
+ * Class FilterCollectionTest
+ *
+ * @package Evheniy\SearchBundle\Tests\Model\Collection
+ */
+class FilterCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testConstruct()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * 
+     */
+    public function testGetEntities()
+    {
+        $this->markTestIncomplete();
+    }
+}
